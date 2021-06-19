9a8c9e2d2f4089c480268557fd8e3a2eedf944ee
osemudiamen Elebesunu <eronscode@gmail.com>
2021-06-20 00:31:58 +0100

    Cors Error Fix

--- server.js
+++ server.js
@@ -3,6 +3,13 @@ const app = express()
 const port = process.env.PORT || 5000
 
 app.use(express.json())
+app.use(function(req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    next();
+});
 
 const subscribersRouter = require('./routes/subscribers')
 app.use('/api/subscribers', subscribersRouter)
