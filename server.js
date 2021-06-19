a3f850584ba291f76a6322d1daf98dac63694636
osemudiamen Elebesunu <eronscode@gmail.com>
2021-06-20 00:07:17 +0100

    Final changes

--- server.js
+++ server.js
@@ -2,10 +2,12 @@ require('dotenv').config()
 
 const express = require('express')
 const app = express()
+const port = process.env.PORT || 5000
 
 app.use(express.json())
 
 const subscribersRouter = require('./routes/subscribers')
 app.use('/api/subscribers', subscribersRouter)
 
-app.listen(9000, () => console.log('Server Started'))
+
+app.listen(port, () => console.log('Server Started'))
